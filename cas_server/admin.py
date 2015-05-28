9a8b724224cc11c6ef74374a5b35d673c3e23dfd
Valentin Samir <valentin.samir@crans.org>
2015-05-28 15:30:51 +0200

    add proxy_callback to admin interface

--- cas_server/admin.py
+++ cas_server/admin.py
@@ -59,7 +59,7 @@ class ServicePatternAdmin(admin.ModelAdmin):
         ReplaceAttributValueInline,
         FilterAttributValueInline
     )
-    list_display = ('pos', 'name', 'pattern', 'proxy', 'single_log_out')
+    list_display = ('pos', 'name', 'pattern', 'proxy', 'single_log_out', 'proxy_callback')
 
 
 admin.site.register(User, UserAdmin)

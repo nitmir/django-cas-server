90e077dedb991d651822e9bb283470de8bddd7dd
Valentin Samir <valentin.samir@crans.org>
2015-12-12 13:29:41 +0100

    typo in setup.cfg

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-cas-server',
-    version='0.2',
+    version='0.2.1',
     packages=['cas_server'],
     include_package_data=True,
     license='GPLv3',

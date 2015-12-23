5e63f39f9b7c678a300ad2f8132166be34d1d35b
Valentin Samir <valentin.samir@crans.org>
2015-12-23 14:43:11 +0100

    Update version to 0.4.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-cas-server',
-    version='0.4',
+    version='0.4.1',
     packages=[
         'cas_server', 'cas_server.migrations',
         'cas_server.management', 'cas_server.management.commands'

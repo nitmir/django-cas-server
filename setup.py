77d1607b0beefe8b171adcd8e2dcd974e3cdc72a
Valentin Samir <valentin.samir@crans.org>
2016-04-30 10:28:26 +0200

    Update version to 0.4.4

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ except pkg_resources.DistributionNotFound:
 
 setup(
     name='django-cas-server',
-    version='0.4.3',
+    version='0.4.4',
     packages=[
         'cas_server', 'cas_server.migrations',
         'cas_server.management', 'cas_server.management.commands'

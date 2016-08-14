cd45c479a8938b0f973165a1a6c8e70c4243066a
Valentin Samir <valentin.samir@crans.org>
2016-08-14 08:21:10 +0200

    Update version to 0.6.4

--- cas_server/__init__.py
+++ cas_server/__init__.py
@@ -11,7 +11,7 @@
 """A django CAS server application"""
 
 #: version of the application
-VERSION = '0.6.3'
+VERSION = '0.6.4'
 
 #: path the the application configuration class
 default_app_config = 'cas_server.apps.CasAppConfig'
